a04f0d19ac104a60936ae4f8846a940f627f3a56
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-10-23 01:41:51 -0500

    constant  the health data

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ import React, { useState, useEffect } from "react";
 
 function App() {
   const [separationStatus, setSeparationStatus] = useState("Pending"); //pending sir
+  const [healthData, setHealthData] = useState({});
 }
