43484a92a99c03a719c962e101dd1e16faec4d52
Joshua Zhuchenko <105894219+jzhuchenko2@users.noreply.github.com>
2024-10-23 01:42:27 -0500

    fetching data from api

--- app.js
+++ app.js
@@ -3,4 +3,5 @@ import React, { useState, useEffect } from "react";
 function App() {
   const [separationStatus, setSeparationStatus] = useState("Pending"); //pending sir
   const [healthData, setHealthData] = useState({});
+  useEffect(() => {
 }
